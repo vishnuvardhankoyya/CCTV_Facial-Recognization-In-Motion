d19a7759db2a65caffb577f8e4554768fc238bde
vishnuvardhankoyya <koyya.vardhan20@st.niituniversity.in>
2023-05-13 10:02:00 +0530

    code mofied in script file

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ function start() {
         //err => console.error(err)
     //)
     
-    video.src = '../videos/frnds.mp4'
+    video.src = '../videos/Year.mp4'
     console.log('video added')
     recognizeFaces()
 }
